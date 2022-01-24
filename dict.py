5b2ab1de8a563fe84c9080f6fb470e48a6b65146
Aarch-code <58585532+Aarch-code@users.noreply.github.com>
2022-01-24 16:00:11 +0530

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,8 @@
+dict1 = {"composed": "unruffled state",
+         "aver": "To allege something",
+         "comprise": "consist of",
+         "wisdom": "knowledge"}
+
+inp1 = input("Enter the word :")
+
+print(dict1[inp1])
